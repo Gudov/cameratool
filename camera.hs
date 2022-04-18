6a2ed1e0948866ddf1df0f695a54a5ea43a26876
Dima Gudov <gudov1134@gmail.com>
2022-04-19 02:03:10 +0700

    looks like shared option

--- camera.hs
+++ camera.hs
@@ -17,13 +17,14 @@ replaceBin nameStr val str =
     in BS.concat [fst splited, fst nullSplit, BSU.pack val, snd $ BS.splitAt valLen $ snd nullSplit]
 
 modConfig original = 
-        modPitchMax $ modVisibleDistance $ modCameraDistanceMin original
+        modMouseWtf $ modPitchMax $ modVisibleDistance $ modCameraDistanceMin original
     where 
         modCameraDistanceMin = replaceBin "dota_camera_distance_min" "1650"
         modVisibleDistance = replaceBin "r_propsmaxdist" "1650"
         modPitchMax = replaceBin "dota_camera_pitch_max" "89"
+        modMouseWtf = replaceBin "dota_camera_mousewheel_direction_multiplier" "89"
 
 main = do
     original <- withBinaryFile "dota 2 beta\\game\\dota\\bin\\win64\\client.dll" ReadMode BS.hGetContents
     let changed = modConfig original
-    withBinaryFile "dota 2 beta\\game\\dota\\bin\\win64\\client.dll" WriteMode $ (\str file -> BS.hPut file str) changed
+    withBinaryFile "dota 2 beta\\game\\dota\\bin\\win64\\client.dll" WriteMode $ (\str file -> BS.hPut file str) changed 
